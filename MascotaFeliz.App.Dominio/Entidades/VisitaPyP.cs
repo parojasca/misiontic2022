1e89e2f305caaa4a73303dc5f17ff29321e1efda
Pedro Rojas <pedro7277@gmail.com>
2022-09-02 18:26:53 -0500

    first commit

---
+++ MascotaFeliz.App.Dominio/Entidades/VisitaPyP.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MascotaFeliz.App.Dominio
+{
+    public class Persona
+    {
+        public int Identificacion {get;set}
+
+        public string Nombre {get;set;}
+
+        public string Apellidos {get;set;}
+
+        public string Direccion {get;set;}
+
+        public string Telefono {get;set;}
+        
+        
+    }
+}
+
